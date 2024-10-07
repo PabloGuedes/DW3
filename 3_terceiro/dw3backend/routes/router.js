9e9f2b77e30a78cf4ef634e3f188d80f6471541c
PabloGuedes <guedespablobacca@gmail.com>
2024-10-07 08:11:54 -0300

    commit

---
+++ 3_terceiro/dw3backend/routes/router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const routerApp = express.Router();
+
+const appLogin = require("../apps/login/controller/ctlLogin");
+
+routerApp.use((req, res, next) => {
+next();
+});
+
+routerApp.get("/", (req, res) => {
+res.send("Olá mundo!");
+});
+
+routerApp.post("/Login", appLogin.Login);
+routerApp.post("/Logout", appLogin.Logout);
+
+module.exports = routerApp;
