3e9cfca3c12f98ec809b2acc72fbc9961a7b05a0
PabloGuedes <guedespablobacca@gmail.com>
2024-10-07 09:37:07 -0300

    Aula 07/10

--- dw3backend/routes/router.js
+++ dw3backend/routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routerApp = express.Router();
 
+const appAlunos = require("../apps/alunos/controller/ctlAlunos");
+const appCursos = require("../apps/cursos/controller/ctlCursos");
 const appLogin = require("../apps/login/controller/ctlLogin");
 
 routerApp.use((req, res, next) => {
@@ -10,6 +12,20 @@ next();
 routerApp.get("/", (req, res) => {
 res.send("Olá mundo!");
 });
+
+
+routerApp.get("/getAllAlunos", appAlunos.getAllAlunos);
+routerApp.post("/getAlunoByID", appLogin.AutenticaJWT, appAlunos.getAlunoByID);
+routerApp.post("/insertAlunos", appLogin.AutenticaJWT, appAlunos.insertAlunos);
+routerApp.post("/updateAlunos", appAlunos.updateAlunos);
+routerApp.post("/DeleteAlunos", appAlunos.DeleteAlunos);
+
+routerApp.get("/GetAllCursos", appCursos.GetAllCursos);
+routerApp.post("/GetCursoByID", appCursos.GetCursoByID);
+routerApp.post("/InsertCursos", appCursos.InsertCursos);
+routerApp.post("/UpdateCursos", appCursos.UpdateCursos);
+routerApp.post("/DeleteCursos", appCursos.DeleteCursos);
+
 
 routerApp.post("/Login", appLogin.Login);
 routerApp.post("/Logout", appLogin.Logout);
